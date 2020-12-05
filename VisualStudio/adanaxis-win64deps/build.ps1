6d59ffad7228cccf3b78c57b53ad591e197b94e7
Andy Southgate <andy@mushware.com>
2020-12-05 15:18:02 +0000

    Download and add SDL libs to output.

--- VisualStudio/adanaxis-win64deps/build.ps1
+++ VisualStudio/adanaxis-win64deps/build.ps1
@@ -40,6 +40,9 @@ $LibpcreName = "pcre-8.44"
 $LibpcreZipName = "$LibpcreName.zip"
 $LibpcreUrl = "https://ftp.pcre.org/pub/pcre/$LibpcreZipName"
 
+$SDLVersion="2.0.12"
+$SDL_mixerVersion="2.0.4"
+
 If ($BuildNumber) {
     If ($BuildNumber -as [int] -gt 65534) {
         Throw "Build number too large"
@@ -82,6 +85,7 @@ if ($PSScriptRoot) {
 $underscore_version = $Version.Replace(".", "_")
 
 $AdanaxisBuildRoot = $(Join-Path -Resolve $ProjectRoot -ChildPath "VisualStudio\adanaxis-win64deps")
+$AdanaxisDownloadRoot = $(Join-Path $ProjectRoot -ChildPath "download")
 $AdanaxisOutRoot = $(Join-Path $ProjectRoot -ChildPath "out")
 $AdanaxisOutName = "adanaxis-win64deps-$Configuration-$underscore_version.zip"
 $AdanaxisOutPath = $(Join-Path $ProjectRoot -ChildPath $AdanaxisOutName)
@@ -138,7 +142,73 @@ If ($null -eq (Get-Command -ErrorAction SilentlyContinue nasm)) {
     Throw "NASM not installed, use e.g. choco install --yes nasm --version 2.14.02"
 }
 
+New-Item -ItemType "directory" -Path $AdanaxisDownloadRoot -Force | Foreach-Object { "Created directory $($_.FullName)" }
 New-Item -ItemType "directory" -Path $AdanaxisOutRoot -Force | Foreach-Object { "Created directory $($_.FullName)" }
+
+
+Write-Host -ForegroundColor DarkCyan @"
+
+*********************************************************************
+*                                                                   *
+*    Fetching libSDL and SDL_mixer                                  *
+*    Props to http://libsdl.org/                                    *
+*                                                                   *
+*********************************************************************
+
+"@
+
+# 
+# Fetch libSDL
+#
+
+$SDLLeafname = "SDL2-devel-${SDLVersion}-VC.zip"
+$SDLUri = "https://www.libsdl.org/release/${SDLLeafname}"
+$SDLTempPath = $(Join-Path $AdanaxisBuildRoot "SDL2-temp")
+$SDLDestPath = $(Join-Path $AdanaxisOutRoot "SDL2")
+$SDLZipPath = $(Join-Path $AdanaxisDownloadRoot $SDLLeafname)
+
+if (Test-Path $SDLDestPath) {
+    Write-Host "libSDL2 already present in $SDLDestPath"
+} else {
+    if (Test-Path $SDLZipPath) {
+        Write-Host "${SDLZipPath} already present so not downloading"
+    } else {
+        Write-Host "Fetching libSDL2 ${SDLVersion} to ${SDLLeafname} from ${SDLUri}"
+        Invoke-WebRequest -Uri $SDLUri -OutFile $SDLZipPath
+    }
+    Remove-Item -Recurse -Force -ErrorAction Ignore $SDLDestPath
+    Expand-Archive $SDLZipPath -DestinationPath $SDLTempPath
+    Move-Item $(Join-Path $SDLTempPath "SDL2-${SDLVersion}") $SDLDestPath
+    Remove-Item $SDLTempPath
+    Write-Host "SDL2 ${SDLVersion} downloaded to ${SDLDestPath}"
+}
+
+#
+# Fetch SDL_mixer
+#
+
+$SDL_mixerLeafname = "SDL2_mixer-devel-${SDL_mixerVersion}-VC.zip"
+$SDL_mixerUri = "https://www.libsdl.org/projects/SDL_mixer/release/${SDL_mixerLeafname}"
+$SDL_mixerTempPath = $(Join-Path $AdanaxisBuildRoot "SDL2_mixer-temp")
+$SDL_mixerDestPath = $(Join-Path $AdanaxisOutRoot "SDL2_mixer")
+$SDL_mixerZipPath = $(Join-Path $AdanaxisDownloadRoot $SDL_mixerLeafname)
+
+if (Test-Path $SDL_mixerDestPath) {
+    Write-Host "libSDL2_mixer already present in $SDL_mixerDestPath"
+} else {
+    if (Test-Path $SDL_mixerZipPath) {
+        Write-Host "${SDL_mixerZipPath} already present so not downloading"
+    } else {
+        Write-Host "Fetching libSDL2_mixer ${SDL_mixerVersion} to ${SDL_mixerZipPath} from ${SDL_mixerUri}"
+        Invoke-WebRequest -Uri $SDL_mixerUri -OutFile $SDL_mixerZipPath
+    }
+
+    Remove-Item -Recurse -Force -ErrorAction Ignore $SDL_mixerDestPath
+    Expand-Archive $SDL_mixerZipPath -DestinationPath $SDL_mixerTempPath
+    Move-Item $(Join-Path $SDL_mixerTempPath "SDL2_mixer-${SDL_mixerVersion}") $SDL_mixerDestPath
+    Remove-Item $SDL_mixerTempPath
+    Write-Host "SDL2_mixer ${SDL_mixerVersion} downloaded to ${SDL_mixerDestPath}"
+}
 
 Write-Host -ForegroundColor DarkCyan @"
 
